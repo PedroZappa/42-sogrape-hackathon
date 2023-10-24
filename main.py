7c3f9d016d86898570104d3399d7000d75bccc40
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-24 14:00:01 +0100

    Removes Scraping Date from Sidebar

--- main.py
+++ main.py
@@ -6,12 +6,13 @@
 #    By: passunca <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/10/24 08:48:10 by passunca          #+#    #+#              #
-#    Updated: 2023/10/24 13:38:14 by passunca         ###   ########.fr        #
+#    Updated: 2023/10/24 13:59:24 by passunca         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import streamlit as st
 import pandas as pd
+import datetime
 
 # Import DB
 df = pd.read_csv("MOCK_DATA2.csv")
@@ -27,9 +28,8 @@ analyser_col1, anaylser_col2 = scrapper_tab.columns(2)
 # Data
 with st.sidebar:
     st.header("Filter Wine Data")
-    st.title("Filter by Date")
+    st.title("Filter by Store")
     st.selectbox('Filter by Store', stores_list)
-
     st.selectbox('Filter by Location', stores_location)
     
     st.title("Filter by Date")
@@ -39,8 +39,6 @@ with st.sidebar:
         max_value=df["Harvest Year"].max(),
         key="harvest-date-slider" 
     )
-
-    st.slider("Scrapping Date", key="scrapping-date-slider" )
 
     # Scrapper TAB
 with scrapper_tab:
